e236cf21fecf1edc22d6e25640ed993c5bc86626
XUAN LIU <142984388+lmsbd@users.noreply.github.com>
2025-05-28 21:12:09 +1000

    Q-learning and my prelinary modify submission

---
+++ agents/t_068 directory/Qlearning1.py
@@ -0,0 +1,129 @@
+# pattern_q_agent.py - Q-learning with tactical pattern features
+from template import Agent
+from Sequence.sequence_model import SequenceGameRule as GameRule, COORDS
+from Sequence.sequence_utils import *
+from copy import deepcopy
+import random, json, os, time
+
+ALPHA = 0.2
+GAMMA = 0.9
+EPSILON = 0.1
+LEARNING = False
+WEIGHT_FILE = "./agents/t_068 directory/weights1.json"
+THINK_TIME_LIMIT = 0.95
+
+FEATURES = [
+    "live_one", "sleep_two", "live_two", "sleep_three", "live_three",
+    "chong_four", "live_four", "live_five",
+    "opp_live_three", "opp_live_four", "opp_live_five"
+]
+
+INIT_WEIGHTS = {f: 0.0 for f in FEATURES}
+
+class myAgent(Agent):
+    def __init__(self, _id):
+        super().__init__(_id)
+        self.id = _id
+        self.rule = GameRule(2)
+        self.weights = INIT_WEIGHTS.copy()
+        self.last_state = None
+        self.last_action = None
+        self.load_weights()
+
+    def SelectAction(self, actions, state):
+        start = time.time()
+
+        if self.last_state and self.last_action:
+            reward = state.agents[self.id].completed_seqs
+            self.update(self.last_state, self.last_action, reward, state)
+
+        best_action = self.choose_action(actions, state, start)
+        self.last_state = deepcopy(state)
+        self.last_action = best_action
+        return best_action
+
+    def choose_action(self, actions, state, start_time):
+        if random.random() < EPSILON:
+            return random.choice(actions)
+        best_score, best_action = float('-inf'), random.choice(actions)
+        for a in actions:
+            if time.time() - start_time > THINK_TIME_LIMIT:
+                break
+            q = self.q_value(state, a)
+            if q > best_score:
+                best_score, best_action = q, a
+        return best_action
+
+    def q_value(self, state, action):
+        feats = self.extract_features(state, action)
+        return sum(self.weights[f] * feats[f] for f in FEATURES)
+
+    def update(self, prev_state, action, reward, next_state):
+        if not LEARNING:
+            return
+        feats = self.extract_features(prev_state, action)
+        pred_q = sum(self.weights[f] * feats[f] for f in FEATURES)
+        next_qs = [self.q_value(next_state, a) for a in self.rule.getLegalActions(next_state, self.id)]
+        max_next_q = max(next_qs) if next_qs else 0.0
+        target = reward + GAMMA * max_next_q
+        for f in FEATURES:
+            self.weights[f] += ALPHA * (target - pred_q) * feats[f]
+        self.save_weights()
+
+    def extract_features(self, state, action):
+        board = [row[:] for row in state.board.chips]
+        me, opp = state.agents[self.id], state.agents[1 - self.id]
+        r, c = action['coords']
+        if action['type'] == 'place':
+            board[r][c] = me.colour
+        elif action['type'] == 'remove':
+            board[r][c] = EMPTY
+        return {
+            "live_one": self.count_pattern(board, me.colour, 1, 2),
+            "sleep_two": self.count_pattern(board, me.colour, 2, 1),
+            "live_two": self.count_pattern(board, me.colour, 2, 2),
+            "sleep_three": self.count_pattern(board, me.colour, 3, 1),
+            "live_three": self.count_pattern(board, me.colour, 3, 2),
+            "chong_four": self.count_pattern(board, me.colour, 4, 1),
+            "live_four": self.count_pattern(board, me.colour, 4, 2),
+            "live_five": self.count_pattern(board, me.colour, 5, 2),
+            "opp_live_three": self.count_pattern(board, opp.colour, 3, 2),
+            "opp_live_four": self.count_pattern(board, opp.colour, 4, 2),
+            "opp_live_five": self.count_pattern(board, opp.colour, 5, 2)
+        }
+
+    def count_pattern(self, board, colour, target_len, open_ends):
+        count = 0
+        for r in range(10):
+            for c in range(10):
+                for dr, dc in [(0, 1), (1, 0), (1, 1), (1, -1)]:
+                    aligned = 0
+                    left_open = right_open = 0
+                    for i in range(target_len):
+                        nr, nc = r + dr * i, c + dc * i
+                        if 0 <= nr < 10 and 0 <= nc < 10 and board[nr][nc] == colour:
+                            aligned += 1
+                        else:
+                            break
+                    if aligned == target_len:
+                        lr, lc = r - dr, c - dc
+                        rr, rc = r + dr * target_len, c + dc * target_len
+                        if 0 <= lr < 10 and 0 <= lc < 10 and board[lr][lc] == EMPTY:
+                            left_open = 1
+                        if 0 <= rr < 10 and 0 <= rc < 10 and board[rr][rc] == EMPTY:
+                            right_open = 1
+                        if left_open + right_open >= open_ends:
+                            count += 1
+        return count
+
+    def save_weights(self):
+        with open(WEIGHT_FILE, 'w') as f:
+            json.dump(self.weights, f)
+
+    def load_weights(self):
+        if os.path.exists(WEIGHT_FILE):
+            with open(WEIGHT_FILE, 'r') as f:
+                self.weights = json.load(f)
+        else:
+            self.weights = INIT_WEIGHTS.copy()
+            self.save_weights()
---
+++ agents/t_068 directory/myTeamheuristic.py
@@ -0,0 +1,207 @@
+from Sequence.sequence_utils import *
+from copy import deepcopy
+import time, random
+from Sequence.sequence_model import SequenceGameRule, COORDS
+
+PLAYERS = 2
+
+class myAgent():
+    def __init__(self, _id):
+        self.id = _id
+        self.game_rule = SequenceGameRule(PLAYERS)
+        self.total_time = 0.0
+        self.max_total_time = 3.0
+        self.timeout_count = 0
+        self.max_timeouts = 3
+        self.corner_coords = [(0, 0), (0, 9), (9, 0), (9, 9)]
+        self.center_coords = [(4, 4), (4, 5), (5, 4), (5, 5)]
+        self.card_value_cache = {}  # 用于缓存卡牌评分
+
+    def register_initial_state(self, state):
+        board = state.board.chips
+        self_color = state.agents[self.id].colour
+        opp_color = state.agents[1 - self.id].colour
+        self.card_value_cache = self.precompute_card_values(board, self_color, opp_color)
+
+    def precompute_card_values(self, board, self_color, opp_color):
+        card_scores = {}
+        for card, positions in COORDS.items():
+            score = 0
+            for r, c in positions:
+                if board[r][c] != EMPTY:
+                    continue
+                if (r, c) in self.center_coords:
+                    score += 50
+                if (r, c) in self.corner_coords:
+                    score += 30
+                if self.near_sequence_check(board, (r, c), self_color, 3):
+                    score += 60
+                if self.near_sequence_check(board, (r, c), opp_color, 3):
+                    score += 80
+            card_scores[card] = score
+        return card_scores
+
+    def evaluate_card_heuristic(self, card, hand, board, self_color, opp_color):
+        if self.card_value_cache and card in self.card_value_cache:
+            return self.card_value_cache[card]
+        if card in ['jc', 'jd']:
+            return self._score_two_eye_joker(board, self_color, opp_color)
+        elif card in ['js', 'jh']:
+            return self._score_one_eye_joker(board, opp_color)
+        return self._score_regular_card(card, hand, board, self_color, opp_color)
+
+    def _score_two_eye_joker(self, board, self_color, opp_color):
+        score = 100
+        for r in range(10):
+            for c in range(10):
+                if board[r][c] == EMPTY:
+                    if (r, c) in self.center_coords:
+                        score += 20
+                    if (r, c) in self.corner_coords:
+                        score += 30
+                    if self.near_sequence_check(board, (r, c), self_color):
+                        score += 40
+                    if self.near_sequence_check(board, (r, c), opp_color):
+                        score += 50
+        return score
+
+    def _score_one_eye_joker(self, board, opp_color):
+        score = 60
+        for r in range(10):
+            for c in range(10):
+                if board[r][c] == opp_color and self.near_sequence_check(board, (r, c), opp_color, min_len=4):
+                    score += 120
+        return score
+
+    def _score_regular_card(self, card, hand, board, self_color, opp_color):
+        score = hand.count(card) * 20
+        for r, c in COORDS[card]:
+            if board[r][c] == EMPTY:
+                score += self._score_position_features((r, c), board, self_color, opp_color)
+        return score
+
+    def _score_position_features(self, pos, board, self_color, opp_color):
+        r, c = pos
+        score = 0
+        if pos in self.corner_coords:
+            score += 70
+        if pos in self.center_coords:
+            score += 60
+        if self.near_sequence_check(board, pos, self_color, 3):
+            score += 90
+        if self.near_sequence_check(board, pos, self_color, 2):
+            score += 50
+        if self.near_sequence_check(board, pos, opp_color, 4):
+            score += 150
+        if self.near_sequence_check(board, pos, opp_color, 3):
+            score += 70
+        score += self.estimate_multi_seq_value(board, pos, self_color)
+        return score
+
+    def near_sequence_check(self, board, pos, color, min_len=4):
+        r, c = pos
+        directions = [(0,1),(1,0),(1,1),(1,-1)]
+        for dr, dc in directions:
+            count = 0
+            for i in range(-4, 5):
+                nr, nc = r + dr*i, c + dc*i
+                if 0 <= nr < 10 and 0 <= nc < 10:
+                    if board[nr][nc] == color or board[nr][nc] == EMPTY:
+                        count += 1
+                        if count >= min_len:
+                            return True
+                    else:
+                        count = 0
+        return False
+
+    def estimate_multi_seq_value(self, board, pos, color):
+        r, c = pos
+        directions = [(0,1),(1,0),(1,1),(1,-1)]
+        value = 0
+        for dr, dc in directions:
+            count, empty = 0, 0
+            for i in range(-4,5):
+                nr, nc = r + dr*i, c + dc*i
+                if 0 <= nr < 10 and 0 <= nc < 10:
+                    if board[nr][nc] == color:
+                        count += 1
+                    elif board[nr][nc] == EMPTY:
+                        empty += 1
+                    else:
+                        count, empty = 0, 0
+                if count + empty >= 4 and count > 0:
+                    value += 20 * count
+        return value
+
+    def simulate_and_score(self, action, state):
+        next_state = deepcopy(state)
+        self.apply_action_to_state(next_state, action, self.id)
+        return next_state.agents[self.id].score
+
+    def SelectAction(self, actions, rootstate):
+        start = time.time()
+        if self.timeout_count >= self.max_timeouts or self.total_time >= self.max_total_time:
+            return random.choice(actions)
+
+        hand_cards = rootstate.agents[self.id].hand
+        available_draft = rootstate.board.draft
+        board = rootstate.board.chips
+        self_color = rootstate.agents[self.id].colour
+        opp_color = rootstate.agents[1 - self.id].colour
+
+        if all(len(self.get_possible_positions(card, board)) == 0 for card in hand_cards):
+            trade_actions = [a for a in actions if a['type'] == 'trade']
+            return random.choice(trade_actions) if trade_actions else random.choice(actions)
+
+        best_draft = max(available_draft, key=lambda c: self.evaluate_card_heuristic(c, hand_cards, board, self_color, opp_color))
+        filtered = [a for a in actions if a['draft_card'] == best_draft] or actions
+
+        chosen = max(filtered, key=lambda a: self.evaluate_card_heuristic(a['draft_card'], hand_cards, board, self_color, opp_color) +
+                                   self.simulate_and_score(a, rootstate) * 100)
+        self.total_time += time.time() - start
+        return chosen
+
+    def get_possible_positions(self, card, board):
+        return [(r, c) for r, c in COORDS.get(card, []) if board[r][c] == EMPTY]
+
+    def apply_action_to_state(self, game_state, action, pid):
+        game_state.board.new_seq = False
+        player = game_state.agents[pid]
+        player.last_action = action
+        score_gain = 0
+        played_card = action['play_card']
+        drafted_card = action['draft_card']
+        if played_card:
+            player.hand.remove(played_card)
+            player.discard = played_card
+            game_state.deck.discards.append(played_card)
+            game_state.board.draft.remove(drafted_card)
+            player.hand.append(drafted_card)
+        row, col = action['coords']
+        if action['type'] == 'place':
+            game_state.board.chips[row][col] = player.colour
+            game_state.board.empty_coords.remove((row, col))
+            game_state.board.plr_coords[player.colour].append((row, col))
+        elif action['type'] == 'remove':
+            game_state.board.chips[row][col] = EMPTY
+            game_state.board.empty_coords.append((row, col))
+            game_state.board.plr_coords[player.opp_colour].remove((row, col))
+        if action['type'] == 'place':
+            result, seq_type = self.game_rule.checkSeq(game_state.board.chips, player, (row, col))
+            if result:
+                score_gain += result['num_seq']
+                game_state.board.new_seq = seq_type
+                for seq_coords in result['coords']:
+                    for r_, c_ in seq_coords:
+                        if game_state.board.chips[r_][c_] != JOKER:
+                            game_state.board.chips[r_][c_] = player.seq_colour
+                            try:
+                                game_state.board.plr_coords[player.colour].remove((r_, c_))
+                            except:
+                                pass
+                player.completed_seqs += result['num_seq']
+                player.seq_orientations.extend(result['orientation'])
+        player.trade = False
+        player.agent_trace.action_reward.append((action, score_gain))
+        player.score += score_gain
+        return game_state
